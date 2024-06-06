c0d71e0163703dbb425ddf13774cbc0a171070b2
JahongirAbdusamatov <jahongir_7440266@mail.ru>
2024-06-06 21:37:28 +0500

    dars

--- Program.cs
+++ Program.cs
@@ -11,3 +11,23 @@ Console.WriteLine("am I learner still = " + amILearner);
 
 
 //4-darsning topshiriqlari
+decimal thirdNumber = 1.5m;
+decimal fourthNumber = 2.5m;
+
+decimal javob = thirdNumber + fourthNumber;
+Console.WriteLine("yechim = " + javob );
+
+decimal foiz = (thirdNumber * 100) / fourthNumber;
+Console.WriteLine("foizi = " + foiz + " %");
+
+// askar
+
+bool isOlder18 = true;
+bool isYounger27 = false;
+
+Console.WriteLine(!isOlder18);
+
+
+
+
+
